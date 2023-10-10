a7a79951eec3de7ee237cd6328e1266e70fe6cef
kwongliik <46083661+kwongliik@users.noreply.github.com>
2023-10-10 22:05:53 +0800

    Update contoh4_11.py 1

--- contoh4_11.py
+++ contoh4_11.py
@@ -1,25 +1,28 @@
-import random
+........ random
 
-# user-defined function untuk simulasi menimbang batu
-def bilangan_batu(x):
-    hasil= random.randint(0, x) # built-in function: randint()
-    return hasil
+# user-defined function bernama bilangan_batu() untuk simulasi menimbang batu
+def ...............(x):
+    hasil= random.randint(0, .....) # built-in function: randint()
+    return ............
 
 # Membuat panggilan function
 # Hantar nilai bilangan batu yang digunakan ke dalam bilangan_batu()
 def main():
     terus = "Y"
     while terus == "Y":
-        hasil_timbang = bilangan_batu(5)
-        print(f"Hasil timbangan anda ialah {hasil_timbang} batu.")
+        hasil_timbang = .............(4)
+        print(f"Hasil timbangan anda ialah {...............} batu.")
 
         # Menangkap batu. Membuat panggilan fungsi
         # Hantar hasil timbangan ke dalam bilangan_batu()
         if hasil_timbang > 0:
-            tangkap = bilangan_batu(hasil_timbang)
-            print(f"Hasil tangkapan anda ialah {tangkap} batu.")
+            tangkap = bilangan_batu(.................)
+            print(f"Hasil tangkapan anda ialah {..............} batu.")
         terus = input("\nTeruskan [Y] atau berhenti [T]? Tekan [Y|T] ")
         print("")
 
+###########################
+# Jangan ubah kod di bawah!
+###########################
 if __name__ == "__main__":
     main()        
